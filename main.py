25f7142c2b78b0e02c70907b84e0f2216e9004e9
Pat Anderson <patrick.b.anderson@gmail.com>
2024-08-15 10:13:17 -0500

    Set up basic file structure and early comments, code still empty

---
+++ main.py
@@ -0,0 +1,14 @@
+#First open an SSH tunnel to NAS
+
+#Get path to folder in question
+
+#Then, get data for photos 
+
+#Use the data to determine the need for folder structure
+
+#Goal is to create folder structure that accounts for
+##Year
+##Month
+##Video Or Photo (one folder for each)
+
+#After folders are made, move photos into folders

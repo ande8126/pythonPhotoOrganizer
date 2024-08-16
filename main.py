1a11fffd3ba1c673ea93ddb91fb0e8c2aa1ae000
Pat Anderson <patrick.b.anderson@gmail.com>
2024-08-16 12:57:05 -0500

    Add logic in main.py into main() function

--- main.py
+++ main.py
@@ -5,42 +5,36 @@ from fileCreator import *
 from photoLibrary import PhotoLibrary
 from videoLibrary import VideoLibrary
 
-#First open an SSH connection to NAS
-print("Have you opened your SSH?")
-isOpen = input("Enter 'y' for Yes or 'n' for No:  ")
+def main():
+    #First open an SSH connection to NAS
+    print("Have you opened your SSH?")
+    isOpen = input("Enter 'y' for Yes or 'n' for No:  ")
 
+    #Get path to folder in question
+    if isOpen == "y":
+        path = input("Copy path of the photos in question here:")
+        if path:
+            #Once you get the path, load the photo and video libraries
+            load_file_libraries(path)
+            new_photo_library = PhotoLibrary.photo_library
+            new_video_library = VideoLibrary.video_library
+            #This method actually moves the files in question
+            #While also creating Year>Month>[Photos, Videos] folders as needed
+            organize_photos(path, new_photo_library, new_video_library)
+        else:
+            print(f"Sorry, path {path} does not exist or is not accessible")
+        #This is just for early testing
+        for photo in new_photo_library:
+            print(photo.file_name)
+            print(photo.year)
+            print(photo.month)
+            print(photo.day)
 
-#Get path to folder in question
-if isOpen == "y":
-    path = input("Copy path of the photos in question here:")
-    if path:
-        load_file_libraries(path)
-        new_photo_library = PhotoLibrary.photo_library
-        new_video_library = VideoLibrary.video_library
-        organize_photos(path, new_photo_library, new_video_library)
-    else:
-        print(f"Sorry, path {path} does not exist or is not accessible")
+        for video in new_video_library:
+            print(video.file_name)
+            print(video.year)
+            print(video.month)
+            print(video.day)
 
-
-#This is just for early testing
-for photo in new_photo_library:
-    print(photo.file_name)
-    print(photo.year)
-    print(photo.month)
-    print(photo.day)
-
-for video in new_video_library:
-    print(video.file_name)
-    print(video.year)
-    print(video.month)
-    print(video.day)
-
-
-#Use the data to determine the need for folder structure
-
-#Goal is to create folder structure that accounts for
-##Year
-##Month
-##Video Or Photo (one folder for each)
-
-#After folders are made, move photos into folders
+if __name__ == "__main__":
+    main()
